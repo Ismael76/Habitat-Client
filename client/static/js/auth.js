e34834c33ae9c15a442321db0a0712a8f3b210e7
Ismael Ali <ismaelali78@outlook.com>
2022-07-19 01:06:43 +0100

    Added heroku URL and changed fetch statements

--- client/static/js/auth.js
+++ client/static/js/auth.js
@@ -1,48 +1,59 @@
-async function requestLogin(e){
-    e.preventDefault();
-    try {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-        }
-        const r = await fetch(`http://localhost:4000/users/login`, options)
-        const data = await r.json()
-        if (data.err){ throw Error(data.err); }
-        login(data);
-    } catch (err) {
-        console.warn(`Error: ${err}`);
+const SERVER_URL = require("./url");
+
+async function requestLogin(e) {
+  const loginData = {
+    email: e.target[0].value,
+    password: e.target[1].value,
+  };
+
+  e.preventDefault();
+  try {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginData),
+    };
+    const r = await fetch(`${SERVER_URL}/users/login`, options);
+    const data = await r.json();
+    if (data.err) {
+      throw Error(data.err);
     }
+    login(data);
+  } catch (err) {
+    console.warn(`Error: ${err}`);
+  }
 }
 
 async function requestRegistration(e) {
-    e.preventDefault();
-    try {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
-        }
-        const r = await fetch(`http://localhost:4000/users/register`, options)
-        const data = await r.json()
-        if (data.err){ throw Error(data.err) }
-        requestLogin(e);
-    } catch (err) {
-        console.warn(err);
+  e.preventDefault();
+  try {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+    };
+    const r = await fetch(`${SERVER_URL}/users/register`, options);
+    const data = await r.json();
+    if (data.err) {
+      throw Error(data.err);
     }
+    requestLogin(e);
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
-function login(data){
-    localStorage.setItem('username', data.user);
-    location.hash = '#habits';
+function login(data) {
+  localStorage.setItem("username", data.user);
+  location.hash = "#habits";
 }
 
-function logout(){
-    localStorage.clear();
-    location.hash = '#login';
+function logout() {
+  localStorage.clear();
+  location.hash = "#login";
 }
 
-function currentUser(){
-    const username = localStorage.getItem('username')
-    return username;
+function currentUser() {
+  const username = localStorage.getItem("username");
+  return username;
 }
--- client/static/js/requests.js
+++ client/static/js/requests.js
@@ -1,10 +1,9 @@
-async function getAllUsers(){
-    try {
-        const response = await fetch('http://localhost:4000/users');
-        const data = await response.json();
-        return data;
-    } catch (err) {
-        console.warn(err);
-    }
-
+async function getAllUsers() {
+  try {
+    const response = await fetch(`${SERVER_URL}/users`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
 }
